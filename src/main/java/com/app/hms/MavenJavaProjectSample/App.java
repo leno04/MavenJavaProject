872f55c1e3cf7f0a9159ced8e6f7917cc998fbde
leno04 <leenasanjana05@gmail.com>
2024-11-22 14:56:20 +0530

    java project

---
+++ src/main/java/com/app/hms/MavenJavaProjectSample/App.java
@@ -0,0 +1,13 @@
+package com.app.hms.MavenJavaProjectSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
